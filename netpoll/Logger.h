6b149335f2cc918901b00bae1f335cbbcca965ba
gongyuandaye <1124095061@qq.com>
2023-11-27 15:18:29 +0800

    feat: finish logger.

---
+++ netpoll/Logger.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <string>
+#include "noncopyable.h"
+
+#define LOG_INFO(logmsgFormat, ...) \
+    { \
+        Logger& logger = Logger::instance(); \
+        logger.setLogLevel(INFO); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__) \
+        logger.log(buf); \
+    }
+
+#define LOG_ERROR(logmsgFormat, ...) \
+    { \
+        Logger& logger = Logger::instance(); \
+        logger.setLogLevel(ERROR); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__) \
+        logger.log(buf); \
+    }
+
+#define LOG_FATAL(logmsgFormat, ...) \
+    { \
+        Logger& logger = Logger::instance(); \
+        logger.setLogLevel(FATAL); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__) \
+        logger.log(buf); \
+    }
+
+#ifdef NETDEBUG
+#define LOG_DEBUG(logmsgFormat, ...) \
+    { \
+        Logger& logger = Logger::instance(); \
+        logger.setLogLevel(FATAL); \
+        char buf[1024] = {0}; \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__) \
+        logger.log(buf); \
+    }
+#else 
+#define LOG_DEBUG(logmsgFormat, ...) 
+#endif
+
+enum LogLevel {
+    INFO,
+    ERROR,
+    FATAL,
+    DEBUG
+};
+
+class Logger: noncopyable {
+public:
+    static Logger& instance();
+    void setLogLevel(int level);
+    void log(std::string msg);
+private:
+    int logLevel_;
+    Logger(){}
+};
